1fcbf87d075d298a4b2dc8416abb46b34e0b0688
nolifer <me@vicentemerlo.com>
2012-11-25 00:37:45 -0200

    added modules support

--- meh.php
+++ meh.php
@@ -1,45 +1,108 @@
 <?php
-	class meh {
-		public static function get($page, $function){
-			self::call($page, 'get', $function);
-		}
+    class meh {
+        public $showErrors   = TRUE;
+        protected $got       = 0;
+        private $baw         = NULL;
 
-		public static function post($page, $function){
-			self::call($page, 'post', $function);
-		}
+        private function __construct(){ }
 
-		public static function any($page, $function){
-			self::call($page, 'any', $function);
-		}
+        public static function start()
+        {
+            return new self;
+        }
 
-		public static function options($page, $function){
-			self::call($page, 'options', $function);
-		}
+        public function modules($modules)
+        {
+            if(!is_null($modules) && is_array($modules))
+            {
+                foreach($modules as $module => $file)
+                {
+                    include $file;
+                    $this->$module = new $module;
+                }   
+            }
+        }
 
-		public static function head($page, $function){
-			self::call($page, 'head', $function);
-		}
+        private function set($wat, $wut)
+        {
+            return $this->$wat = $wut;
+        }
 
-		public static function put($page, $function){
-			self::call($page, 'put', $function);
-		}
+        public function get($page, $function)
+        {
+            self::call($page, 'get', $function);
+        }
 
-		public static function delete($page, $function){
-			self::call($page, 'delete', $function);
-		}
+        public function post($page, $function)
+        {
+            self::call($page, 'post', $function);
+        }
 
-		public static function trace($page, $function){
-			self::call($page, 'trace', $function);
-		}
+        public function any($page, $function){
+            self::call($page, 'any', $function);
+        }
 
-		public static function connect($page, $function){
-			self::call($page, 'connect', $function);
-		}
+        public function options($page, $function)
+        {
+            self::call($page, 'options', $function);
+        }
 
-		private static function call($page, $method, $function){
-			if(preg_match('/'.$page.'/i', $_SERVER['REQUEST_URI'], $param) && (strtolower($_SERVER['REQUEST_METHOD']) == $method || $method == 'any')){
-				echo call_user_func($function, $param); 
-			}
-		}
-	}
-?>
+        public function head($page, $function)
+        {
+            self::call($page, 'head', $function);
+        }
+
+        public function put($page, $function)
+        {
+            self::call($page, 'put', $function);
+        }
+
+        public function delete($page, $function)
+        {
+            self::call($page, 'delete', $function);
+        }
+
+        public function trace($page, $function)
+        {
+            self::call($page, 'trace', $function);
+        }
+
+        public function connect($page, $function)
+        {
+            self::call($page, 'connect', $function);
+        }
+
+        public function baw($why = 404, $function = null)
+        {
+            $this->baw = $function;
+        }
+
+        public function __destruct()
+        {
+            if($this->got == 0 && $this->showErrors == true)
+            {
+                if(is_null($this->baw))
+                {
+                    $this->baw = function()
+                    {
+                        return '<h1>bawwwww ;-;</h1>';
+                    };
+                }
+                echo call_user_func($this->baw);
+            }
+        }
+
+        private function call($page, $method, $function)
+        {
+            $page = str_replace(array(':anything:', ':numbers:', ':letters:', ':number:', ':letter:', '/'), array('(.*)', '([0-9]+)', '([a-zA-Z]+)', '([0-9])', '([a-zA-Z])', '\/'), $page);
+            if(preg_match('/'.$page.'\/'.'/i', $_SERVER['REQUEST_URI'].'/', $param) && (strtolower($_SERVER['REQUEST_METHOD']) == $method || $method == 'any'))
+            {
+                $this->got++;
+                if(count($param) <3)
+                {
+                    $param = $param[1];
+                }
+                echo call_user_func($function, $param); 
+            }
+        }
+    }
